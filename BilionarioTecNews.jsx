7473a21fb554dc5031d295983a543f9156929791
Jodalmo21 <jodalmogunzagunza@gmail.com>
2025-06-08 08:37:09 -0300

    Add files via upload

---
+++ BilionarioTecNews.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BilionarioTecNews() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Bilionário Tec. News</h1>
+      <p>As maiores novidades do mundo da tecnologia e dos bilionários.</p>
+    </div>
+  );
+}
